b96c5b14998d71f54943a37ff0637f242ef09769
suyalamritanshu <amritanshu.suyall2@gmail.com>
2022-05-07 15:06:25 +0530

    Added component

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
+import React, {useEffect, useState}from 'react';
 import { StyleSheet, Text, View, ImageBackground } from 'react-native';
-const img = require('./assets/image.png')
-import DateTime from './components/DateTime';
-import FutureForecast from './components/FutureForecast';
-import WeatherScroll from './components/WeatherScroll';
+import * as Location from 'expo-location';
 
+import DateTime from './components/DateTime'
+import WeatherScroll from './components/WeatherScroll'
+const API_KEY ='c19fa19ef11b4b6afb77be7c89d883b0';
+const img = require('./assets/image.png')
 export default function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        fetchDataFromApi("40.7128", "-74.0060")
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      fetchDataFromApi(location.coords.latitude, location.coords.longitude);
+    })();
+  }, [])
+
+  const fetchDataFromApi = (latitude, longitude) => {
+    if(latitude && longitude) {
+      fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`).then(res => res.json()).then(data => {
+
+      // console.log(data)
+      setData(data)
+      })
+    }
+    
+  }
+
   return (
     <View style={styles.container}>
-      <ImageBackground source={img} style={styles.image}>
-
-        <DateTime />
-        <WeatherScroll />
-
-        <FutureForecast />
+      <ImageBackground source={img} style={styles.image} >
+        <DateTime current={data.current} timezone={data.timezone} lat={data.lat} lon={data.lon}/>
+        <WeatherScroll weatherData={data.daily}/>
       </ImageBackground>
-
     </View>
   );
 }
@@ -24,5 +48,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  image: { flex: 1, resize: "cover", justifyContent: "center" }
+  image:{
+    flex:1, 
+    resizeMode:"cover", 
+    justifyContent:"center"
+  }
 });
--- components/DateTime.js
+++ components/DateTime.js
@@ -1,5 +1,11 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import moment from 'moment-timezone'
 import { View, Text, StyleSheet } from 'react-native'
+
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
 
 const WeatherItem = ({ title, value, unit }) => {
     return (
@@ -11,28 +17,48 @@ const WeatherItem = ({ title, value, unit }) => {
     )
 }
 
-const DateTime = () => {
+const DateTime = ( {current, lat, lon, timezone}) => {
+    const [date, setDate] = useState("")
+    const [time, setTime] = useState("")
+
+    useEffect(() => {
+        setInterval(() => {
+            const time = new Date();
+            const month = time.getMonth();
+            const date = time.getDate();
+            const day = time.getDay();
+            const hour = time.getHours();
+            const hoursIn12HrFormat = hour >= 13 ? hour % 12 : hour
+            const minutes = time.getMinutes();
+            const ampm = hour >= 12 ? 'pm' : 'am'
+
+            setTime((hoursIn12HrFormat < 10 ? '0' + hoursIn12HrFormat : hoursIn12HrFormat) + ':' + (minutes < 10 ? '0' + minutes : minutes) + ampm)
+
+            setDate(days[day] + ', ' + date + ' ' + months[month])
+
+        }, 1000);
+    }, [])
+
     return (
         <View style={styles.container}>
             <View>
                 <View>
-                    <Text style={styles.heading}>12:30AM</Text>
+                    <Text style={styles.heading}>{time}</Text>
                 </View>
                 <View>
-                    <Text style={styles.subheading}>Monday, June 7</Text>
+                    <Text style={styles.subheading}>{date}</Text>
                 </View>
                 <View style={styles.weatherItemContainer}>
-                    <WeatherItem title="Humidity" value="45" unit="%" />
-                    <WeatherItem title="Humidity" value="45" unit="%" />
-                    <WeatherItem title="Humidity" value="45" unit="%" />
-                    <WeatherItem title="Humidity" value="45" unit="%" />
-                    <WeatherItem title="Humidity" value="45" unit="%" />
+                    <WeatherItem title="Humidity" value={current ? current.humidity : ""} unit="%" />
+                    <WeatherItem title="Pressure" value={current ? current.pressure : ""} unit="hPA" />
+                    <WeatherItem title="Sunrise" value={current ? moment.tz(current.sunrise * 1000, timezone).format('HH:mm') : ""} unit="am" />
+                    <WeatherItem title="Sunset" value={current ? moment.tz(current.sunset * 1000, timezone).format('HH:mm') : ""} unit="pm" />
 
                 </View>
             </View>
             <View style={styles.rightAlign}>
-                <Text style={styles.timezone}>Asia/Kolkata</Text>
-                <Text style={styles.latlong}>343432</Text>
+                <Text style={styles.timezone}>{timezone}</Text>
+                <Text style={styles.latlong}>{lat}N {lon}E</Text>
             </View>
         </View>
     )
@@ -84,7 +110,7 @@ const styles = StyleSheet.create({
         fontSize: 14,
         fontWeight: "100",
     },
- 
+
 
 })
 
